a7a864d9e0059b0b4e3c4df71cd682af0bc4aa15
ram3ez <rameez12m@gmail.com>
2025-12-30 14:06:08 +0400

    output-standalone

--- gyanith/next.config.ts
+++ gyanith/next.config.ts
@@ -2,20 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   compiler: {
-    styledJsx: true
+    styledJsx: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        pathname: '**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "**",
       },
     ],
   },
 
-  allowedDevOrigins: ['*'],
+  allowedDevOrigins: ["*"],
 };
 
 export default nextConfig;
